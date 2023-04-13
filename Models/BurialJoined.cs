e25ef1c716fb602ddedd45465af39c0caeb70415
Jacob Sargent <jrsarge@byu.edu>
2023-04-13 17:09:51 -0600

    BurialJoined Model

---
+++ Models/BurialJoined.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntexScratch.Models
+{
+    public class BurialJoined
+    {
+        public long burialid { get; set; }
+        public int? squarenorthsouth { get; set; }
+        public string? headdirection { get; set; }
+        public string? sex { get; set; }
+        public string? northsouth { get; set; }
+        public float? depth { get; set; }
+        public string? eastwest { get; set; }
+        public string? adultsubadult { get; set; }
+        public string? southtohead { get; set; }
+        public string? preservation { get; set; }
+        public string? fieldbookpage { get; set; }
+        public int? squareeastwest { get; set; }
+        public string? goods { get; set; }
+        public string? text { get; set; }
+        public string? wrapping { get; set; }
+        public string? haircolor { get; set; }
+        public string? westtohead { get; set; }
+        public string? samplescollected { get; set; }
+        public string? area { get; set; }
+        public string? length { get; set; }
+        public string? burialmain_burialnumber { get; set; }
+        public string? dataexpertinitials { get; set; }
+        public string? westtofeet { get; set; }
+        public string? ageatdeath { get; set; }
+        public string? southtofeet { get; set; }
+        public string? excavationrecorder { get; set; }
+        public string? photos { get; set; }
+        public string? hair { get; set; }
+        public string? burialmaterials { get; set; }
+        public string? fieldbookexcavationyear { get; set; }
+        public string? clusternumber { get; set; }
+        public string? shaftnumber { get; set; }
+        public long? burialmain_linking_burialmainid { get; set; }
+        public long? burialmain_linking_textileid { get; set; }
+        public string? textileid { get; set; }
+        public string? description { get; set; }
+        public string? textile_burialnumber { get; set; }
+        public string? estimatedperion { get; set; }
+        public string? sampledate { get; set; }
+        public string? photographeddate { get; set; }
+        public string? direction { get; set; }
+        public string? textilefunction_linking_textilefunctionid { get; set; }
+        public string? textilefunction_linking_textileid { get; set; }
+        public string? textilefunctionid { get; set; }
+        public string? color_linking_colorid { get; set; }
+        public string? color_linking_textileid { get; set; }
+        public string? color_main_id { get; set; }
+        public string? color_value { get; set; }
+        public string? colorid { get; set; }
+        public string? structure_linking_structureid { get; set; }
+        public string? structure_linking_textile { get; set; }
+        public string? structure_main_id { get; set; }
+        public string? structure_value { get; set; }
+        public string? structureid { get; set; }
+
+    }
+}
